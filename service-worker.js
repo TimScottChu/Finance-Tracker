92738778f505e0416e0434149bc2309edd10a760
TimScottChu <TimScottChu@users.noreply.github.com>
2026-07-22 14:43:02 +0800

    Simplify summary recurring total

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "php-finance-tracker-v27";
+const CACHE_NAME = "php-finance-tracker-v28";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=27",
-  "./app.js?v=27",
+  "./styles.css?v=28",
+  "./app.js?v=28",
   "./manifest.webmanifest",
   "./icons/icon.svg"
 ];
